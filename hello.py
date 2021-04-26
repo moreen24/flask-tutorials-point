b15c197787edcd528650f58509a09a6371b1d470
moreen24 <64901499+moreen24@users.noreply.github.com>
2021-04-27 00:42:53 +0300

    added at

--- hello.py
+++ hello.py
@@ -10,11 +10,11 @@ def hello_world():
 def home():
    return "This is home"
 
-app.route('/produts<name>')
+@app.route('/produts<name>')
 def get_products(name):
     return "the product name is " + str(name)
 
-app.route('/create/<first_name>/<last_name>')
+@app.route('/create/<first_name>/<last_name>')
 def create(first_name=None,last_name=None):
     return 'hello' + first_name +","+ last_name    
 
