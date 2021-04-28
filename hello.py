f5cfa1f2b3ea06f4be14cddfb140df0905078f01
moreen24 <64901499+moreen24@users.noreply.github.com>
2021-04-28 13:08:49 +0300

    url routes

--- hello.py
+++ hello.py
@@ -26,6 +26,21 @@ def hello_flask():
 def hello_python():
    return 'helo python'
 
+@app.route('/admin')
+def hello_admin(admin):
+   return'hello admin'
+
+@app.route ('/guest/<guest>')
+def hello_guest(guest): 
+   return 'hello %s as guest' %guest
+
+@app.route('/user/<name>')
+def hello_user(name):
+  if name == admin:
+   return redirect(url_for('hello_admin'))
+  else:
+   return redirect (url_for ('hello_guest', guest= name))      
+
 if __name__ == '__main__':
    app.run()
 
