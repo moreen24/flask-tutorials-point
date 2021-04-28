6325a8d594cedb0f2f4eafc1d991f00166874448
moreen24 <64901499+moreen24@users.noreply.github.com>
2021-04-28 12:32:18 +0300

    changed the route

--- variables.py
+++ variables.py
@@ -1,9 +1,13 @@
 from flask import Flask
 app =Flask(__name__)
 
-@app.route('/hello/<name>')
-def hello_name(name):
-    return 'hello %s!' %name
+@app.route('/blog/<int:post_id>')
+def show_blog(post_id):
+    return 'blog_number %d!' %id
+
+@app.route('/rev/<float:rev')
+def revision(revNo):
+    return 'Revision number %f' %revNo
 
 
 if __name__ == '__main__':
