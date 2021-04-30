6efc48654f94fd3516f59dfa38bed992fc602a16
moreen24 <64901499+moreen24@users.noreply.github.com>
2021-04-30 23:18:54 +0300

    changes

--- hello.py
+++ hello.py
@@ -1,15 +1,14 @@
-from flask import Flask
-
+from flask import Flask, redirect, url_for
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-   return "Hello World"
+# @app.route('/')
+# def hello_world():
+#    return "Hello World"
 
 
-@app.route('/home')
-def home():
-   return "This is home"
+# @app.route('/home')
+# def home():
+#    return "This is home"
 
 @app.route('/produts/<name>')
 def get_products(name):
@@ -41,7 +40,22 @@ def hello_user(name):
    return redirect(url_for('hello_admin'))
   else:
      return redirect(url_for('hello_guest',guest=name))
-     
+
+@app.route('/')
+def index():
+   return 'the index'
+
+@app.route('/first')
+def first_route():
+   return url_for('index') 
+
+@app.route('/second')
+def second_route():
+  return redirect(url_for('index'))  
+
+@app.route('/third/<string:name>')
+def third_route(name):
+   return 'the name is' + name
 if __name__ == '__main__':
     app.run()
 
